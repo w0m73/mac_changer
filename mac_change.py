a1d4692dde11f859a10c502ffa3d7f3dc77b61ac
_rəSH_ <127571364+w0m73@users.noreply.github.com>
2023-04-01 21:14:55 -0300

    Add files via upload

---
+++ mac_change.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+import subprocess  # Essas funções nos permitem executar comandos do sistema.
+
+subprocess.call("ifconfig eth0 down", shell=True)
+subprocess.call("ifconfig eth0 hw ether 00:11:22:33:44:55", shell=True)
+subprocess.call("ifconfig eth0 up", shell=True)
+
